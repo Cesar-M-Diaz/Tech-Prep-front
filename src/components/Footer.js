b2cad28024de9d5c9be57f0790ff39c48c4f1456
cesar diaz <arq.cmdiaz1@gmail.com>
2021-11-01 19:13:18 -0500

    Create Navbar for monile and desktop

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../assets/styles/components/Footer.scss';
+
+function Footer() {
+  return (
+    <section className="footer-body">
+      <div className="footer-line"></div>
+      <div className="footer-text__container">
+        <p>2021 Tech-prep</p>
+        <p>built for the Dev Community</p>
+        <p>github repo</p>
+      </div>
+    </section>
+  );
+}
+
+export default Footer;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="home__page-body">
+      <p>sdfsd</p>
+    </div>
+  );
+}
+
+export default HomePage;
