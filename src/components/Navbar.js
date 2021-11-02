b6b87ab99968d271133863bb4a7ba01f75039521
cesar diaz <arq.cmdiaz1@gmail.com>
2021-11-01 21:27:41 -0500

    Add secondary navbar functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,40 +8,53 @@ import { IconContext } from 'react-icons';
 
 function Navbar({ toggleMenu }) {
   const [menu, setMenu] = useState(false);
+  const [toggleSecondaryMenu, setToggleSecondaryMenu] = useState(false);
 
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
         <div className="header__wrapper">
-          <section className="header__container">
-            <div className="header-logo__container">
-              <img
-                src={logo}
-                alt=""
-                className="header__logo"
-                onClick={() => history.push('/home')}
-              />
-            </div>
-            <div className="header__menu__container">
-              {!menu ? (
-                <FiMenu className="header__menu" onClick={() => toggleMenu(setMenu(!menu))} />
-              ) : (
-                <MdClose className="header__menu" onClick={() => toggleMenu(setMenu(!menu))} />
-              )}
-            </div>
-            <div className="header-links__container">
-              <p>train</p>
-              <p>add questions</p>
-              <p>resources</p>
-              <img src="" alt="" />
-            </div>
-          </section>
+          <div className="header__menu__container">
+            {!menu ? (
+              <FiMenu className="header__menu" onClick={() => toggleMenu(setMenu(!menu))} />
+            ) : (
+              <MdClose className="header__menu" onClick={() => toggleMenu(setMenu(!menu))} />
+            )}
+          </div>
           <div className={`header-links__container-mobile${menu ? '-active' : '-hidden'}`}>
             <img src="" alt="" />
+            <p>profile</p>
+            <p>logout</p>
             <p>train</p>
             <p>add questions</p>
             <p>resources</p>
           </div>
+          <section className="header__container-wrapper">
+            <div className="header__container">
+              <div className="header-logo__container">
+                <img
+                  src={logo}
+                  alt=""
+                  className="header__logo"
+                  onClick={() => history.push('/home')}
+                />
+              </div>
+              <div className="header-links__container">
+                <p>train</p>
+                <p>add questions</p>
+                <p>resources</p>
+                <img src="" alt="" onClick={() => setToggleSecondaryMenu(!toggleSecondaryMenu)} />
+              </div>
+            </div>
+            <div
+              className={`header-secondary-links__container${
+                toggleSecondaryMenu ? '-active' : '-hidden'
+              }`}
+            >
+              <p>profile</p>
+              <p>logout</p>
+            </div>
+          </section>
         </div>
       </IconContext.Provider>
     </>
