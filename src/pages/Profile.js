524f1d6b2119f487b28a6ba001a6d6dd621a5d47
cesar diaz <arq.cmdiaz1@gmail.com>
2021-11-02 20:38:46 -0500

    Add mobile styles

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -67,72 +67,74 @@ function Profile() {
       <div className="profile__backgroud-shape-2"></div>
       <div className="profile__backgroud-shape-1"></div>
       <form className="profile__card-body" onSubmit={handleSubmit(onSubmit)}>
-        <div className="profile__photo-container">
-          <img src={previewPhoto} alt="" />
-          <label htmlFor="upload" className="profile__button-photo">
-            upload photo
-          </label>
-          <input
-            type="file"
-            id="upload"
-            onChange={onChangeFile}
-            hidden
-            accept="image/png, image/jpeg"
-          />
-        </div>
-        <div className="profile__text-container">
-          <h1>hello internet citizen this is your tech-prep id</h1>
-          <div className="profile__input-container">
+        <h1>hello internet citizen this is your tech-prep id</h1>
+        <div className="profile__card-body-container">
+          <div className="profile__photo-container">
+            <img src={previewPhoto} alt="" />
+            <label htmlFor="upload" className="profile__button-photo">
+              upload photo
+            </label>
             <input
-              type="text"
-              disabled={isDisabled.name}
-              {...register('name', {
-                required: true,
-                pattern:
-                  /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/g,
-              })}
-              placeholder="Name"
+              type="file"
+              id="upload"
+              onChange={onChangeFile}
+              hidden
+              accept="image/png, image/jpeg"
             />
+          </div>
+          <div className="profile__text-container">
+            <div className="profile__input-container">
+              <input
+                type="text"
+                disabled={isDisabled.name}
+                {...register('name', {
+                  required: true,
+                  pattern:
+                    /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/g,
+                })}
+                placeholder="Name"
+              />
+              <button className="profile__button-edit-name" onClick={handleClick}>
+                edit
+              </button>
+            </div>
             {(errors.name?.type === 'required' && <p>Name is required</p>) ||
               (errors.name?.type === 'pattern' && <p>Name must only contain letters</p>)}
-            <button className="profile__button-edit-name" onClick={handleClick}>
-              edit
-            </button>
-          </div>
-          <div className="profile__input-container">
-            <input
-              type="email"
-              disabled={isDisabled.email}
-              {...register('email', {
-                required: true,
-                pattern:
-                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-              })}
-              placeholder="Email"
-            />
+            <div className="profile__input-container">
+              <input
+                type="email"
+                disabled={isDisabled.email}
+                {...register('email', {
+                  required: true,
+                  pattern:
+                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                })}
+                placeholder="Email"
+              />
+              <button className="profile__button-edit-email" onClick={handleClick}>
+                edit
+              </button>
+            </div>
             {(errors.email?.type === 'required' && <p>Email is required</p>) ||
               (errors.email?.type === 'pattern' && <p>Please enter a valid email</p>)}
-            <button className="profile__button-edit-email" onClick={handleClick}>
-              edit
-            </button>
-          </div>
-          <div className="profile__input-container">
-            <input
-              disabled={isDisabled.password}
-              type="password"
-              {...register('password', {
-                required: true,
-                minLength: 5,
-              })}
-              placeholder="Password"
-            />
+            <div className="profile__input-container">
+              <input
+                disabled={isDisabled.password}
+                type="password"
+                {...register('password', {
+                  required: true,
+                  minLength: 5,
+                })}
+                placeholder="Password"
+              />
+              <button className="profile__button-edit-password" onClick={handleClick}>
+                edit
+              </button>
+            </div>
             {(errors.password?.type === 'required' && <p>Password is required</p>) ||
               (errors.password?.type === 'minLength' && <p>Password is to short</p>)}
-            <button className="profile__button-edit-password" onClick={handleClick}>
-              edit
-            </button>
+            <button className="profile__button-submit">save changes</button>
           </div>
-          <button className="profile__button-submit">save changes</button>
         </div>
       </form>
     </main>
