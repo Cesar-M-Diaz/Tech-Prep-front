86fbcc52b3f9ceace1a85c69fbb34d238e81db31
cesar diaz <arq.cmdiaz1@gmail.com>
2021-11-07 20:24:55 -0500

    Almost complete frontend basic implementation

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import GameCard from '../components/GameCard';
 import Slider from 'react-slick';
+import history from '../utils/history';
 import { FaArrowRight, FaArrowLeft } from 'react-icons/fa';
-import '../assets/styles/pages/ResultPage.scss';
+import '../assets/styles/pages/GamePage.scss';
 // import soundtrack from '../assets/audio/soundtrack.mp3';
 
 function ResultPage() {
   // const [result, setResult] = useState(12);
   // const [questionsNumber, setQuestionsNumber] = useState(15);
-  // const [page, setPage] = useState(1);
   const [imageIndex, setImageIndex] = useState(0);
-  // const [answers, setAnswers] = useState('');
-
+  const [answers, setAnswers] = useState({});
+  const [wrongAnswers, setWrongAnswers] = useState([]);
   const NextArrow = ({ onClick }) => {
     return (
       <div className="arrow next" onClick={onClick}>
@@ -49,10 +49,11 @@ function ResultPage() {
     ],
   };
 
-  const answers = [
+  const mockQuestions = [
     {
       id: 1,
-      option_1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      answer: 'answer Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      option_1: 'answer Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       option_2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       option_3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       question:
@@ -60,7 +61,8 @@ function ResultPage() {
     },
     {
       id: 2,
-      option_1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      answer: 'answer Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      option_1: 'answer Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       option_2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       option_3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       question:
@@ -68,7 +70,8 @@ function ResultPage() {
     },
     {
       id: 3,
-      option_1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      answer: 'answer Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      option_1: 'answer Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       option_2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       option_3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       question:
@@ -76,34 +79,63 @@ function ResultPage() {
     },
     {
       id: 4,
-      option_1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      answer: 'answer Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      option_1: 'answer Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       option_2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       option_3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       question:
         '4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure mollitia doloremque inventore accusamus quae quod cumque voluptate dolorem labore ea.',
     },
-    {
-      id: 5,
-      option_1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      option_2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      option_3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      question:
-        '5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure mollitia doloremque inventore accusamus quae quod cumque voluptate dolorem labore ea.',
-    },
   ];
 
+  function assignAnswers(answer, status) {
+    setAnswers((prev) => ({ ...prev, [answer]: status }));
+  }
+
+  function submitAnswers() {
+    Object.keys(answers).forEach((answer) => {
+      if (answers[answer] === 'wrong') {
+        setWrongAnswers((prev) => [...prev, answer]);
+      }
+    });
+    history.push('/result');
+  }
+  console.log(wrongAnswers);
+  console.log(answers);
+
   return (
-    <div className="game-questions-container">
-      <button>audio on</button>
-      <button>quit game</button>
-      <button>finish</button>
-      <Slider {...settings}>
-        {answers?.map((answer, idx) => (
-          <div className={idx === imageIndex ? 'game-slide-active-slide' : 'game-slide'}>
-            <GameCard data={answer} flip={idx === imageIndex} />
-          </div>
-        ))}
-      </Slider>
+    <div className="game-page">
+      <div className="game-top">
+        <h1>time left 10:00 min</h1>
+      </div>
+      <div className="game-questions-container">
+        <Slider {...settings}>
+          {mockQuestions?.map((answer, idx) => (
+            <div className={idx === imageIndex ? 'game-slide-active-slide' : 'game-slide'}>
+              <GameCard data={answer} flip={idx === imageIndex} assignAnswers={assignAnswers} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+      <div className="game-controls">
+        <div
+          className={
+            Object.keys(answers).length === mockQuestions.length
+              ? 'game__button-container'
+              : 'game__button-container-hidden'
+          }
+          onClick={submitAnswers}
+        >
+          <div className="game__collapsible-a"></div>
+          <p>check answers</p>
+          <div className="game__collapsible-b"></div>
+        </div>
+        <div className="game__button-container" onClick={() => history.push('/train')}>
+          <div className="game__collapsible-a"></div>
+          <p>quit game</p>
+          <div className="game__collapsible-b"></div>
+        </div>
+      </div>
     </div>
   );
 }
